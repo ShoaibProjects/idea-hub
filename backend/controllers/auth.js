64354e8a326ab6760557982b2ff2e5e7584eb82b
ShoaibProjects <shoaibakhtarcs@gmail.com>
2024-11-06 07:39:26 +0530

    try 6

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -124,7 +124,7 @@ export const signin = async (req, res) => {
       secure: true,
       maxAge: req.body.rememberMe ? COOKIE_MAX_AGE : COOKIE_MAX_AGE_30_MINUTES,
       sameSite: 'None',
-      domain: 'https://idea-hub-project.vercel.app'
+      domain: '.vercel.app'
     });
 
     // Set the "Remember Me" cookie if the option is checked
@@ -134,7 +134,7 @@ export const signin = async (req, res) => {
         secure: true,
         maxAge: COOKIE_MAX_AGE, // 7 days expiration for the rememberMe cookie
         sameSite: 'None',
-        domain: 'https://idea-hub-project.vercel.app'
+        domain: '.vercel.app'
       });
     }
     
