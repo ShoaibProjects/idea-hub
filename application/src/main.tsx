92ccd6004d5eef4458e29c41e15615e9407bb359
ShoaibProjects <shoaibakhtarcs@gmail.com>
2024-12-15 12:52:21 +0530

    trying analytics

--- application/src/main.tsx
+++ application/src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store.ts';
 import App from './App.tsx'
 import './index.scss'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
   //   </Provider>
   // </StrictMode>,
   <Provider store={store}>
+    <Analytics/>
      <App />
     </Provider>,
 )
