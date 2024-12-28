2103229a3cf27a760e309ebc2df03e6613b159fa
ShoaibProjects <shoaibakhtarcs@gmail.com>
2024-12-28 12:29:50 +0530

    res try 9

--- application/server.js
+++ application/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 // app.use(cors());
 app.use(cors({
   // origin: 'http://localhost:5173',  // Frontend URL
-  origin: ['https://idea-hub-app.vercel.app'],
+  origin: ['https://idea-hub-app.vercel.app','http://localhost:5173'],
   credentials: true,                // Allow cookies
 }));
 app.use(json());
