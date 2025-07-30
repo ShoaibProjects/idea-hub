3fecdf8bb7f693d614752a3fd2f76ef0d509bc4e
ShoaibProjects <shoaibakhtarcs@gmail.com>
2025-07-31 01:32:09 +0530

    changes

--- application/services/searchService.js
+++ application/services/searchService.js
@@ -2,6 +2,8 @@ import Idea from "../models/Idea.js";
 import User from "../models/User.js";
 
 export const performSearch = async (query, page = 1, limit = 10) => {
+    if (isNaN(page) || page < 1) page = 1;
+    if (isNaN(limit) || limit < 1) limit = 10;
     const skip = (page - 1) * limit;
 
     const ideaSearchPromise = Idea.aggregate([
@@ -15,17 +17,24 @@ export const performSearch = async (query, page = 1, limit = 10) => {
                                 query,
                                 path: "title",
                                 score: { boost: { value: 3 } },
+                                fuzzy: {
+                                    maxEdits: 1,
+                                    prefixLength: 2,
+                                },
                             },
                         },
                         {
-                            text: { query, path: "description" },
+                            text: {
+                                query,
+                                path: "description",
+                                fuzzy: {
+                                    maxEdits: 1,
+                                    prefixLength: 2,
+                                },
+                            },
                         },
                     ],
                     minimumShouldMatch: 1,
-                },
-                fuzzy: {
-                    maxEdits: 1,
-                    prefixLength: 2,
                 },
             },
         },
